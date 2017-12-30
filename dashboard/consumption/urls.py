cec21363e7fd1edf4262efe39d61918561a040b4
lutrasdebtra <stuy.bradley@gmail.com>
2017-12-30 18:37:05 +1300

    SMAP Challenge now feature complete.

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.summary),
-    url(r'^summary/', views.summary),
-    url(r'^detail/', views.detail),
+    url(r'^summary/', views.summary, name='summary'),
+    url(r'^detail/(?P<pk>\d+)$', views.detail, name='detail'),
 ]
--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import json
 
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import *
@@ -12,16 +13,24 @@ from .models import *
 
 
 def summary(request):
-    data = json.dumps(
-        [[str(x[0]), x[1]] for x in TimePointAggregateData.objects.values_list('time_point', 'average')[::48]])
+    aggregate_data = json.dumps([[str(x[0]), x[1]] for x in
+                                 TimePointAggregateData.objects.values_list('time_point', 'average')[25::48]])
     context = {
         'message': 'Hello!',
-        'time_point_aggregate_data': data
+        'time_point_aggregate_data': aggregate_data,
+        'user_data': UserData.objects.all()
     }
     return render(request, 'consumption/summary.html', context)
 
 
-def detail(request):
-    context = {
-    }
+def detail(request, pk):
+    context = {}
+    try:
+        context['user_data'] = UserData.objects.get(pk=pk)
+        context['time_points'] = json.dumps([[str(x[0]), x[1]] for x in
+                                             context['user_data'].consumptiontimepoint_set.all().values_list(
+                                                 'time_point', 'consumption')[25::48]])
+        context['time_points_non_json'] = context['user_data'].consumptiontimepoint_set.all()
+    except UserData.DoesNotExist:
+        raise Http404
     return render(request, 'consumption/detail.html', context)
