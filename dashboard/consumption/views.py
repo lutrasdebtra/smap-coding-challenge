8883c6f623dc010cc382d4651d696e570438974b
lutrasdebtra <stuy.bradley@gmail.com>
2017-12-30 18:42:07 +1300

    Added page info to jumbotron.

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -18,19 +18,26 @@ def summary(request):
     context = {
         'message': 'Hello!',
         'time_point_aggregate_data': aggregate_data,
-        'user_data': UserData.objects.all()
+        'user_data': UserData.objects.all(),
+        'page': 'Summary Page'
     }
     return render(request, 'consumption/summary.html', context)
 
 
 def detail(request, pk):
-    context = {}
     try:
-        context['user_data'] = UserData.objects.get(pk=pk)
-        context['time_points'] = json.dumps([[str(x[0]), x[1]] for x in
-                                             context['user_data'].consumptiontimepoint_set.all().values_list(
+        user = UserData.objects.get(pk=pk)
+        time_points = json.dumps([[str(x[0]), x[1]] for x in
+                                  user.consumptiontimepoint_set.all().values_list(
                                                  'time_point', 'consumption')[25::48]])
-        context['time_points_non_json'] = context['user_data'].consumptiontimepoint_set.all()
+        time_points_non_json = user.consumptiontimepoint_set.all()
     except UserData.DoesNotExist:
         raise Http404
+    context = {
+        'user_data': user,
+        'time_points': time_points,
+        'time_points_non_json': time_points_non_json,
+        'page': 'User {} Detail Page'.format(user.id)
+
+    }
     return render(request, 'consumption/detail.html', context)
