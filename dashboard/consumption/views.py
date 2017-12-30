534785699a2deb9f97e2575cfa8bd0e4994c977e
lutrasdebtra <stuy.bradley@gmail.com>
2017-12-30 19:42:28 +1300

    Moved datatables loader to external file.

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -9,14 +9,14 @@ from django.shortcuts import render
 from .models import *
 
 
-# Create your views here.
-
-
 def summary(request):
+    """ View for a summary of consumption data. """
+
+    # Gets every 12pm TimePointAggregateData object. Also converts it into a format readable by Charts.js
+    # Before converting to JSON.
     aggregate_data = json.dumps([[str(x[0]), x[1]] for x in
                                  TimePointAggregateData.objects.values_list('time_point', 'average')[25::48]])
     context = {
-        'message': 'Hello!',
         'time_point_aggregate_data': aggregate_data,
         'user_data': UserData.objects.all(),
         'page': 'Summary Page'
@@ -25,12 +25,17 @@ def summary(request):
 
 
 def detail(request, pk):
+    """ View for a single user. """
     try:
         user = UserData.objects.get(pk=pk)
-        time_points = json.dumps([[str(x[0]), x[1]] for x in
-                                  user.consumptiontimepoint_set.all().values_list(
-                                                 'time_point', 'consumption')[25::48]])
         time_points_non_json = user.consumptiontimepoint_set.all()
+        # Gets every 12pm ConsumptionTimePoint object. Also converts it into a format readable by Charts.js
+        # Before converting to JSON.
+        time_points = json.dumps([[str(x[0]), x[1]] for x in
+                                  time_points_non_json.values_list(
+                                                 'time_point', 'consumption')[25::48]])
+        # Also gets a
+
     except UserData.DoesNotExist:
         raise Http404
     context = {
